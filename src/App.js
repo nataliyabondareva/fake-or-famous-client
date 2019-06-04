ae6d510835743357ae7bcbe2160ec15df0e7e7fc
tallybee <talbondareva@gmail.com>
2019-06-04 13:54:11 +0200

    set up pages, some actions reducers

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import { Provider } from 'react-redux'
 import { Route } from 'react-router-dom'
 import Home from './components/Home'
 import LoginFormContainer from './components/LoginFormContainer'
+import SignupForm from './components/SignupForm';
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <div>
-          <Route path="/" exact component={LoginFormContainer} />
+          <Route path="/logins" exact component={LoginFormContainer} />
           <Route path="/" exact component={Home} />
+          <Route path="/signup" exact component={SignupForm} />
         </div>
       </Provider>
     );
