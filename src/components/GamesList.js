921cecf50999d61eedd7a9670b2357cbf31fedd0
Cathal <hastinc@tcd.ie>
2019-06-05 14:14:34 +0200

    render and redirects

--- src/components/GamesList.js
+++ src/components/GamesList.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function GamesLists(props) {
+  console.log(props)
   const { games } = props
   const GamesList = !games
     ? "Loading ... "
     : games.map(game =>
       <li key={game.id}>
         <Link to={`/games/${game.id}`}>
-          {game.name}
+          {game.status}
         </Link>
       </li >)
 
