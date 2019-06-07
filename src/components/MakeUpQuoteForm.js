9f157451e0de336e93679530e2b7bf114567419d
tallybee <talbondareva@gmail.com>
2019-06-07 16:56:12 +0200

    done

--- src/components/MakeUpQuoteForm.js
+++ src/components/MakeUpQuoteForm.js
@@ -4,10 +4,10 @@ export default function MakeUpQuoteForm(props) {
   return (
   <form onSubmit={props.onSubmit}>
     <label>
-      Quote:
+      Tell us:
       <input type='text' name='content' value={props.values.content} onChange={props.onChange}/>
     </label>
-    <button type='submit'>FAKE IT </button>
+    <button type='submit'>LAUGH </button>
 
   </form>)
 }
